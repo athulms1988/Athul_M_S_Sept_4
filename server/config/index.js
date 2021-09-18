a80743a8e4aeecdaf08bd1bd70d1cb025b459dc8
Athul MS <athulms@gmail.com>
2021-09-18 08:41:59 +0530

    Dockerize the application

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,5 @@
 module.exports.DB_CONFIG = {
-    connectionUrl: 'mongodb+srv://admin:EFl1ic14kvVMwesJ@oracle-todo.gj7sk.mongodb.net/oracle-todo?retryWrites=true&w=majority'
+    connectionUrl: 'mongodb://mongo:27017/mongo-test'
   }
   module.exports.JWT_TOKEN_ALGORITHM = 'HS256'
   module.exports.JWT_TOKEN_SECRET = '1c8fcf99-d508-4dc0-b9c8-1ca4a3be4e3a'
